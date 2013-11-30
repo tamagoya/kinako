1bcfb83e397489f6f89593b5df991c29297c1ac2
Takahiro <studio.quad9@gmail.com>
2013-11-30 16:38:33 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kinako::Application.config.secret_key_base = '81680c8805dbfec2c2017f76b6d77995c34af862fe636208a90228794f1eaaf62ace71a7af7438fedfa9259a4c4900bf2d1d9dfac728470a0d37be1d4628cf06'
