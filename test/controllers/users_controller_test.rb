529de9880f9a802ac9c1581ab8c523f9c7c83cca
yalab <rudeboyjet@gmail.com>
2013-12-07 00:55:11 +0900

    users#create is not use any more.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -16,14 +16,6 @@ class UsersControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should create user" do
-    assert_difference('User.count') do
-      post :create, user: { email: @user.email, kidlemail: @user.kidlemail, name: @user.name }
-    end
-
-    assert_redirected_to user_path(assigns(:user))
-  end
-
   test "should show user" do
     get :show, id: @user
     assert_response :success
